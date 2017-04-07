a40826611b968e8967a580351f0a3172b4dfc248
patrickof <patrickofboy@hotmail.com>
2017-04-07 12:36:12 -0300

    Add files via upload

---
+++ b/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned char s1, s2, s3, m1, m2;
+
+    return 0;
+}
---
+++ lab/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+
+
+int main()
+
+{
+
+   int n1, n2, n3;
+
+   printf("Digite tres numeros:\n");
+
+   scanf("%d %d %d", &n1, &n2, &n3 );
+
+   if (n1>n2){
+
+       if (n2>n3){
+
+        printf("%d %d %d", n2, n3, n1);//n1>n2>n3
+
+                 }
+       if(n3>n1){
+
+            printf("%d %d %d", n2, n1, n3);//n3>n1>n2
+
+       }
+
+       else{
+
+        printf("%d %d %d", n2, n3, n1);//n1>n3>n2
+
+           }
+
+             }
+
+   else{//n2>=n1
+
+       if (n1>n3){
+
+                printf("%d %d %d", n3, n1, n2);//n2>n1>n3
+
+
+                 }
+
+        else{//n1<n3
+
+        printf("%d %d %d", n1, n3, n2);//n2>n3>n1
+
+            }
+        if(n3>n2){
+
+            printf("%d %d %d", n1, n2, n3); //n3>n2>n1
+
+        }
+
+       }
+
+ return 0;
+
+}
